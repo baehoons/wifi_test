7890c09a8ff83ce7ce6ac651bfdfc1c4fe309d19
baehoons <qogns314@naver.com>
2020-03-02 07:39:37 +0900

    ss

--- app/src/main/java/com/baehoons/wifitest/ScanFragment.kt
+++ app/src/main/java/com/baehoons/wifitest/ScanFragment.kt
@@ -1,17 +1,86 @@
 package com.baehoons.wifitest
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_scan.*
 import kotlinx.android.synthetic.main.item_wifi_scan.*
 
 
 class ScanFragment : Fragment() {
+    private lateinit var managePermissions: ManagePermissions
+    private val requestCode  = 123
+    private val TAG = "WIFI_TAG"
+    private var resultList = ArrayList<ScanResult>()
+    lateinit var adapter: WifiAdapter
     var navController: NavController?=null
+
+    private val wifiManager: WifiManager
+        get() = context?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    private val wifiReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            unregisterReceiver(this)
+            Log.d(TAG, "unregisterReceiver")
+
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            Log.d(TAG, "intent.getBooleanExtra")
+            if (success) {
+                scanSuccess()
+                Log.d(TAG, "scan successful")
+            }
+            else {
+                scanFailure()
+                Log.d(TAG, "scan failed")
+            }
+
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        button_scan.setOnClickListener {
+            adapter.submitList(arrayListOf())
+            scanWifiNetworks()
+        }
+        wifiManager = context?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        adapter = WifiAdapter()
+        recycler_view.layoutManager = LinearLayoutManager(context)
+        recycler_view.adapter = adapter
+    }
+
+    private fun scanWifiNetworks() {
+        registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+        val success = wifiManager.startScan()
+        if (!success) {
+            scanFailure()
+        }
+        Toast.makeText(context, "Scanning", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun scanFailure() {
+        Toast.makeText(context, "Scan failure", Toast.LENGTH_SHORT).show()
+    }
+    private var wifiReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d(TAG, "onReceive " + wifiManager.scanResults.size)
+            unregisterReceiver(this)
+            adapter.submitList(wifiManager.scanResults)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +88,7 @@ class ScanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_scan,container,false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/baehoons/wifitest/WifiAdapter.kt
+++ app/src/main/java/com/baehoons/wifitest/WifiAdapter.kt
@@ -1,13 +1,46 @@
 package com.baehoons.wifitest
 
+import android.annotation.SuppressLint
 import android.net.wifi.ScanResult
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.item_wifi_scan.view.*
 
-class WifiAdapter {
+class WifiAdapter  : ListAdapter<ScanResult, WifiAdapter.ViewHolder>(DIFF_UTIL) {
 
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder.create(p0)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.text_wifi_name.text = getItem(position).SSID
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        companion object {
+            fun create(parent: ViewGroup): ViewHolder {
+                return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_wifi_scan,  parent,false))
+            }
+        }
+    }
+
+    companion object {
+        val DIFF_UTIL = object : DiffUtil.ItemCallback<ScanResult>() {
+
+            override fun areItemsTheSame(p0: ScanResult, p1: ScanResult): Boolean {
+                return false
+            }
+
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(p0: ScanResult, p1: ScanResult): Boolean {
+                return p0 == p1
+            }
+        }
+    }
 }
